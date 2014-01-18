e7eaeb8ebf0ac58dd3ffa2e70d3458b365a90c83
bibinette <sabineberets4@msn.com>
2014-01-18 20:42:09 +0100

    verdion without print

--- app/views.py
+++ app/views.py
@@ -20,9 +20,7 @@ def string2_to_list(Thestring2):
     Thestring2 = map(int,Thestring2.split(","))
     return Thestring2
 
-#def display_list_guess(list_guess):
-#   a1= list_guess[0], a2= list_guess[1], a3= list_guess[2], a4= list_guess[3], a5= list_guess[4], a6= list_guess[5], a7= list_guess[6], a8= list_guess[7], a9= list_guess[8], a10= list_guess[9]
-    
+
 def set_cookies(respExit, r, list_guess, list_red, list_white):
     respExit.set_cookie('red', '', expires=0)
     respExit.set_cookie('white', '', expires=0)
@@ -63,10 +61,7 @@ def index():
     r = request.cookies.get('r')
     if r == None:
         r = 0
-    print list_guess
-    print list_red
-    print list_white
-    print "boo"
+    
     Secret = request.cookies.get('Secret')
     if Secret == None:
         print "none"
@@ -84,15 +79,8 @@ def index():
         respEntry.set_cookie('list_red', list_to_string2(list_red))
         respEntry.set_cookie('list_white', list_to_string2(list_white))
         respEntry.set_cookie('r', str(r))
-        print list_to_string(Secret)
         return respEntry    
-    
-    print str(red)    
-    print list_guess 
-    print list_red
-    print list_white
-    
-    print "YET"      
+      
     
     # get cookies
     if request.method =='GET':
@@ -102,12 +90,7 @@ def index():
         
     # get guess
     guess = request.form['Guess']
-    
-    
-    
-    print Secret
-    print guess
-     
+         
     red_place = [0,1,2,3] 
     white_place = [0,1,2,3] 
                 
@@ -126,43 +109,30 @@ def index():
     generalist = []
     for i in range(0,r):
         generalist.append([attempt_number[i], list_guess[i], list_red[i], list_white[i]])
-        print generalist 
-        
+               
     if guess.isdigit() and len(guess) == 4: 
         guess = string_to_list(guess)
         for i in range (0,4):
             if guess[i] == 0 or guess[i] == 9:
                 return render_template("index.html",
                 title = 'Beanmind', r =r, red = red, Attempt_Number= ' Attempt Number ', Guess_form = ' Guess ', Number_of_Reds = ' Number of Reds ', Number_of_Whites = ' Number of Whites ', attempt_number = attempt_num, generalist = generalist, check_guess = "Du, Dummkopf! Please read carefully! I said:"
-                )
-        
+                )        
     else:   
         return render_template("index.html",
         title = 'Beanmind', r = r, red = red, Attempt_Number= ' Attempt Number ', Guess_form = ' Guess ', Number_of_Reds = ' Number of Reds ', Number_of_Whites = ' Number of Whites ', attempt_number = attempt_num, generalist = generalist, check_guess = "Du, Dummkopf! Please read carefully! I said:"
         )
-    
-    
-    print Secret
-    print guess
-    
-    
+       
     # checking for the reds
     for i in range(0, 4):  
-        print "vous"
-        print guess[i]
-        print Secret[i]
-        
-        if guess[i] == Secret[i]:
-            print "yeah"
-            print guess[i]
-            print Secret[i]
+       
+        if guess[i] == Secret[i]:           
             red_place.remove(i)    
             white_place.remove(i)
             red = red + 1 
-            print red
+
         if red == 4:
             respExit = make_response(render_template("index.html",
-                title = 'Beanmind',first = " Yeah you're so good!", red = red, r = r ))
+                title = 'Beanmind',fourth = " Yeah you're so good!", red = red, r = r ))
             respExit.set_cookie('Secret','', expires=0)
             respExit.set_cookie('red', '', expires=0)
             respExit.set_cookie('white', '', expires=0)
@@ -175,35 +145,23 @@ def index():
                 
     # checking for whites   
     for j in red_place:
-        print "lui"
         for h in white_place:
-            print"elle"
-            print guess[j]
-            print Secret[h]
             if guess[j] == Secret[h]: 
                 white_place.remove(h)
                 white = white + 1
-                print"nous"
                 break
             
     # result
-    print red
-    print white
     list_guess[r] = int(list_to_string (guess))
     list_red[r] = red
     list_white[r] = white
     r = r + 1     
-    print list_guess
-    print list_red
-    print list_white
     
     attempt_number = range(1,11)
     attempt_num = range(0,r)
-    print attempt_number
     generalist = []
     for i in range(0,r):
-        generalist.append([attempt_number[i], list_guess[i], list_red[i], list_white[i]])
-        print generalist   
+        generalist.append([attempt_number[i], list_guess[i], list_red[i], list_white[i]]) 
     
     if r==10 and red < 4:
         respExit = make_response(render_template("index.html",
